66972095e71b389bc2858c6087421dc3547d1c62
James King <james@agentultra.com>
2024-04-02 22:13:18 -0400

    Add overlaps
    Checks if two Patterns, p1 and p2, overlap when p2 is offset by 1 in
    the Direction specified.

--- src/Algorithm/WaveFunctionCollapse.hs
+++ src/Algorithm/WaveFunctionCollapse.hs
@@ -1,11 +1,13 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Algorithm.WaveFunctionCollapse where
 
-import Data.Array (Array)
-import Data.Foldable
+import Data.Array (Array, (!))
 import qualified Data.Array as Array
+import Data.Foldable
+import Data.Maybe
 import Test.QuickCheck
 
 data Pattern a
@@ -23,6 +25,11 @@ mkFillPattern fillValue size
   . Pattern size
   . Array.listArray ((0,0), (size, size))
   $ repeat fillValue
+
+patternValue :: Pattern a -> Word -> Word -> Maybe a
+patternValue p x y
+  | x < patternSize p && y < patternSize p = Just $ getPattern p ! (x, y)
+  | otherwise = Nothing
 
 newtype Texture a = Texture { getTexture :: Array (Word, Word) a }
   deriving (Eq, Show)
@@ -67,3 +74,32 @@ patterns texture subPatternSize
 
     accumElems :: Array (Word, Word) a -> [a] -> (Word, Word) -> [a]
     accumElems tex acc ix = (tex Array.! ix) : acc
+
+data Direction
+  = Up
+  | Right'
+  | Down
+  | Left'
+  deriving (Eq, Show)
+
+-- | Check if two patterns overlap, offset in a given Direction by 1.
+overlaps :: Eq a => Pattern a -> Pattern a -> Direction -> Bool
+overlaps p1 p2 = \case
+  Up ->
+    let p1Values = catMaybes [ patternValue p1 x y | x <- [0..patternSize p1 - 1], y <- [0..patternSize p1 - 2] ]
+        p2Values = catMaybes [ patternValue p2 x y | x <- [0..patternSize p1 - 1], y <- [1..patternSize p1 - 1] ]
+    in all elemEq $ zip p1Values p2Values
+  Right' ->
+    let p1Values = catMaybes [ patternValue p1 x y | x <- [0..patternSize p1 - 2], y <- [0..patternSize p1 - 1] ]
+        p2Values = catMaybes [ patternValue p2 x y | x <- [0..patternSize p2 - 2], y <- [0..patternSize p2 - 1] ]
+    in all elemEq $ zip p1Values p2Values
+  Down ->
+    let p1Values = catMaybes [ patternValue p1 x y | x <- [0..patternSize p1 - 1], y <- [1..patternSize p1 - 1] ]
+        p2Values = catMaybes [ patternValue p2 x y | x <- [0..patternSize p2 - 1], y <- [0..patternSize p2 - 2] ]
+    in all elemEq $ zip p1Values p2Values
+  Left' ->
+    let p1Values = catMaybes [ patternValue p1 x y | x <- [1..patternSize p1 - 2], y <- [0..patternSize p1 - 1] ]
+        p2Values = catMaybes [ patternValue p2 x y | x <- [1..patternSize p2 - 1], y <- [0..patternSize p2 - 1] ]
+    in all elemEq $ zip p1Values p2Values
+  where
+    elemEq (x, y) = x == y
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 
+import Control.Monad
+import qualified Data.Array as Array
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Algorithm.WaveFunctionCollapse
@@ -9,3 +12,30 @@ main = hspec $ do
   describe "patterns" $ do
     prop "all overlapping patterns" $ \(tex :: Texture Int) ->
       (length $ patterns tex 3) `shouldBe` fromIntegral ((textureSize tex) * (textureSize tex))
+
+  describe "overlaps" $ do
+    context "A fill pattern" $ do
+      it "should always overlap with itself in every direction" $ do
+        let (Just fillPattern) = mkFillPattern (0 :: Int) 3
+        forM_ [Up, Right', Down, Left'] $ \dir -> do
+          overlaps fillPattern fillPattern dir `shouldBe` True
+
+    context "Known overlapping patterns" $ do
+      let p1 = Pattern @Int 3
+             $ Array.listArray ((0,0), (2,2))
+             [ 1, 1, 3
+             , 1, 1, 3
+             , 1, 1, 3
+             ]
+          p2 = Pattern @Int 3
+             $ Array.listArray ((0,0), (2,2))
+             [ 3, 1, 1
+             , 3, 1, 1
+             , 3, 1, 1
+             ]
+      it "should overlap on Up" $ do
+        overlaps p1 p2 Up `shouldBe` True
+
+      it "should not overlap on any others" $ do
+        forM_ [Right', Down, Left'] $ \dir -> do
+          overlaps p1 p2 dir `shouldBe` False
