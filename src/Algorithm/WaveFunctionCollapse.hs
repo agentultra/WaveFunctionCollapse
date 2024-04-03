bb475110dbfe42fe6b906ee99e909d4208891f9c
James King <james@agentultra.com>
2024-04-02 21:08:13 -0400

    Modify patterns
    Extract all overlapping patterns from texture, treating the texture as
    a toroid (infinite surface).

--- src/Algorithm/WaveFunctionCollapse.hs
+++ src/Algorithm/WaveFunctionCollapse.hs
@@ -6,6 +6,7 @@ module Algorithm.WaveFunctionCollapse where
 import Data.Array (Array)
 import Data.Foldable
 import qualified Data.Array as Array
+import Test.QuickCheck
 
 data Pattern a
   = Pattern
@@ -25,6 +26,12 @@ mkFillPattern fillValue size
 
 newtype Texture a = Texture { getTexture :: Array (Word, Word) a }
   deriving (Eq, Show)
+
+instance Arbitrary a => Arbitrary (Texture a) where
+  arbitrary = do
+    s <- chooseInt (1, 100)
+    xs <- infiniteList
+    pure . Texture $ Array.listArray ((0, 0), (toEnum s - 1, toEnum s - 1)) $ take (s * s) xs
 
 mkTexture :: a -> Word -> Texture a
 mkTexture fillValue size
@@ -46,16 +53,15 @@ patterns :: Texture a -> Word -> [Pattern a]
 patterns texture subPatternSize
   | subPatternSize == 0 = []
   | otherwise =
-    let numPatterns =
-          (textureSize texture - subPatternSize)
+    let upTo = textureSize texture - 1
     in [ extractPattern texture (x, y) subPatternSize
-       | x :: Word <- [0..numPatterns], y :: Word <- [0..numPatterns]
+       | x :: Word <- [0..upTo], y :: Word <- [0..upTo]
        ]
   where
     extractPattern :: Texture a -> (Word, Word) -> Word -> Pattern a
     extractPattern (Texture tex) (x, y) size =
       let w = size - 1
-          indices = [ (a, b) | a <- [x..x+w], b <- [y..y+w] ]
+          indices = [ (a `mod` w, b `mod` w) | a <- [x..x+w], b <- [y..y+w] ]
           textureElems = foldl' (accumElems tex) [] indices
       in Pattern size $ Array.listArray ((0, 0), (size - 1, size - 1)) textureElems
 
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,11 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Algorithm.WaveFunctionCollapse
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "patterns" $ do
+    prop "all overlapping patterns" $ \(tex :: Texture Int) ->
+      (length $ patterns tex 3) `shouldBe` fromIntegral ((textureSize tex) * (textureSize tex))
