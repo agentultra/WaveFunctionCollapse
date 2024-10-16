4ab97a09426fb4f887670e9935265d512231b4f5
James King <james@agentultra.com>
2024-10-15 22:02:11 -0400

    Add wfc-demo executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,52 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Main (main) where
 
-import Lib
+import Control.Monad (unless)
+import Linear (V4(..))
+import Options.Applicative
+import SDL
+import qualified SDL.Image as Image
+import System.FilePath
+
+newtype Options
+  = Options
+  { image :: FilePath
+  }
+  deriving (Eq, Show)
+
+parseOptions :: Parser Options
+parseOptions = do
+  image <- strOption (long "image")
+  pure Options {..}
 
 main :: IO ()
-main = someFunc
+main = do
+  options <- execParser $ info parseOptions fullDesc
+  img <- Image.load options.image
+  print options
+  initializeAll
+  Image.initialize [Image.InitPNG]
+  window <- createWindow "My SDL Application" defaultWindow
+  renderer <- createRenderer window (-1) defaultRenderer
+  appLoop img renderer
+  destroyWindow window
+
+appLoop :: Surface -> Renderer -> IO ()
+appLoop imgSurface renderer = do
+  events <- pollEvents
+  let eventIsQPress event =
+        case eventPayload event of
+          KeyboardEvent keyboardEvent ->
+            keyboardEventKeyMotion keyboardEvent == Pressed &&
+            keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
+          _ -> False
+      qPressed = any eventIsQPress events
+  rendererDrawColor renderer $= V4 0 0 255 255
+  clear renderer
+  -- TODO (james): render the input surface
+  present renderer
+  unless qPressed (appLoop imgSurface renderer)
