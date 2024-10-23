eba5975541c06cc2691fa0ca3d93527934068496
James King <james@agentultra.com>
2024-10-22 22:03:55 -0400

    Add WFC Texture from Surface loading

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main (main) where
 
-import Control.Monad (unless)
-import Linear (V4(..))
+import qualified Algorithm.WaveFunctionCollapse as WFC
+import Control.Exception
+import Control.Monad (unless, when)
+import Foreign.C.Types
+import Foreign.Ptr
+import Foreign.Storable
 import Options.Applicative
 import SDL
 import qualified SDL.Image as Image
-import System.FilePath
 
 newtype Options
   = Options
@@ -23,6 +27,9 @@ parseOptions = do
   image <- strOption (long "image")
   pure Options {..}
 
+withSurface :: Surface -> (Surface -> IO a) -> IO a
+withSurface surface = bracket (lockSurface surface >> pure surface) unlockSurface
+
 main :: IO ()
 main = do
   options <- execParser $ info parseOptions fullDesc
@@ -30,8 +37,17 @@ main = do
   Image.initialize [Image.InitPNG]
   window <- createWindow "My SDL Application" defaultWindow
   renderer <- createRenderer window (-1) defaultRenderer
-  img <- Image.loadTexture renderer options.image
-  appLoop img renderer
+  img <- Image.load options.image
+  inputTexture <- withSurface img $ \imgSurface -> do
+    (V2 imgW imgH) <- surfaceDimensions imgSurface
+    when (imgW == imgH) $ error "Input image must be square"
+    pixelPtr <- surfacePixels imgSurface
+    pixels <- sequence [getPixel pixelPtr x | x <- [0..(imgW*imgH) - 1]]
+    pure $ WFC.textureFromList (fromIntegral imgW) pixels
+
+  print inputTexture
+  imgTexture <- createTextureFromSurface renderer img
+  appLoop imgTexture renderer
   destroyWindow window
 
 appLoop :: Texture -> Renderer -> IO ()
@@ -46,7 +62,11 @@ appLoop imgTexture renderer = do
       qPressed = any eventIsQPress events
   rendererDrawColor renderer $= V4 0 0 255 255
   clear renderer
-  -- TODO (james): render the input surface
   copy renderer imgTexture Nothing Nothing
   present renderer
   unless qPressed (appLoop imgTexture renderer)
+
+getPixel :: Ptr () -> CInt -> IO Word
+getPixel voidPixelPtr ix = do
+  let pixelPtr = castPtr @() @Word voidPixelPtr
+  peekElemOff pixelPtr (fromIntegral ix)
