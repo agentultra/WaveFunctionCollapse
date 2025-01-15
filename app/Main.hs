f1c0d2d70a4916b16550f87f335bdeec33046ff2
James King <james@agentultra.com>
2025-01-14 22:03:49 -0500

    Modify app/Main.hs
    Update setPixel and getPixel offsets

--- app/Main.hs
+++ app/Main.hs
@@ -44,21 +44,24 @@ main = do
 
   inputTexture <- fromSurface img
 
-  print inputTexture
+  img' <- toSurface inputTexture
+  imgTexture <- createTextureFromSurface renderer img'
+  appLoop imgTexture renderer
 
-  let patternResults = WFC.patterns inputTexture 3
-      inputWaveState = WFC.mkWaveState (15, 15) 100 patternResults
-      outputWaveState = WFC.runWave inputWaveState WFC.collapseWave
+  -- let patternResults = WFC.patterns inputTexture 3
+  --     inputWaveState = WFC.mkWaveState (15, 15) 100 patternResults
+  --     outputWaveState = WFC.runWave inputWaveState WFC.collapseWave
 
-  case WFC.mkOutputTexture patternResults outputWaveState of
-    Left err -> putStrLn err >> destroyWindow window
-    Right outputWFCTexture -> do
-      print outputWFCTexture
-      img' <- toSurface outputWFCTexture
+  -- case WFC.mkOutputTexture patternResults outputWaveState of
+  --   Left err -> putStrLn err >> destroyWindow window
+  --   Right outputWFCTexture -> do
+  --     print outputWFCTexture
+  --     --img' <- toSurface outputWFCTexture
+  --     img' <- toSurface inputTexture
 
-      imgTexture <- createTextureFromSurface renderer img'
-      appLoop imgTexture renderer
-      destroyWindow window
+  --     imgTexture <- createTextureFromSurface renderer img'
+  --     appLoop imgTexture renderer
+  --     destroyWindow window
 
 appLoop :: Texture -> Renderer -> IO ()
 appLoop imgTexture renderer = do
@@ -76,38 +79,43 @@ appLoop imgTexture renderer = do
   present renderer
   unless qPressed (appLoop imgTexture renderer)
 
-fromSurface :: Surface -> IO (WFC.Texture CUInt)
+-- 32-bit packed pixel format R G B A
+fromSurface :: Surface -> IO (WFC.Texture CInt)
 fromSurface img = withSurface img $ \imgSurface -> do
   (V2 imgW imgH) <- surfaceDimensions imgSurface
   print (imgW, imgH)
   unless (imgW == imgH) $ error "Input image must be square"
   pixelPtr <- surfacePixels imgSurface
   pixels <- sequence [getPixel imgSurface pixelPtr x y
-                     | x <- [0..imgW - 1]
-                     , y <- [0..imgH - 1]
+                     | y <- [0..imgW - 1]
+                     , x <- [0..imgH - 1]
                      ]
   pure $ WFC.textureFromList (fromIntegral imgW) pixels
 
-toSurface :: WFC.Texture CUInt -> IO Surface
+toSurface :: WFC.Texture CInt -> IO Surface
 toSurface wfcTexture = do
   let (_, (tw, th)) = Array.bounds wfcTexture.getTexture
-      twI = fromIntegral tw - 1
   output <- createRGBSurface (fromIntegral <$> V2 tw th) RGBA8888
   bracket_ (lockSurface output) (unlockSurface output) $ do
     voidPixelPtr <- surfacePixels output
-    let pixelPtr = castPtr @() @CUInt voidPixelPtr
-    sequence_ [setPixel pixelPtr ((fromIntegral y * twI) + fromIntegral x) v | ((x, y), v) <- Array.assocs wfcTexture.getTexture]
+    let pixelPtr = castPtr @() @CInt voidPixelPtr
+    sequence_ [setPixel output pixelPtr (fromIntegral x) (fromIntegral y) v | ((x, y), v) <- Array.assocs wfcTexture.getTexture]
   pure output
 
-getPixel :: Surface -> Ptr () -> CInt -> CInt -> IO CUInt
+getPixel :: Surface -> Ptr () -> CInt -> CInt -> IO CInt
 getPixel (Surface surfacePtr _) voidPixelPtr x y = do
   -- https://github.com/haskell-game/sdl2/issues/175
   rawSurface <- peek surfacePtr
   rawFormat <- peek $ Raw.surfaceFormat rawSurface
   let bpp = rawFormat.pixelFormatBytesPerPixel
-      pixelPtr = castPtr @() @CUInt voidPixelPtr
+      pixelPtr = castPtr @() @CInt voidPixelPtr
       pitch = Raw.surfaceW rawSurface * fromIntegral bpp
   peekByteOff pixelPtr (fromIntegral y * fromIntegral pitch + fromIntegral x * fromIntegral bpp)
 
-setPixel :: Ptr CUInt -> Int -> CUInt -> IO ()
-setPixel = pokeByteOff
+setPixel :: Surface -> Ptr CInt -> CInt -> CInt -> CInt -> IO ()
+setPixel (Surface surfacePtr _) pixelPtr x y pixelData = do
+  rawSurface <- peek surfacePtr
+  rawFormat <- peek $ Raw.surfaceFormat rawSurface
+  let bpp = rawFormat.pixelFormatBytesPerPixel
+      pitch = Raw.surfaceW rawSurface * fromIntegral bpp
+  pokeByteOff pixelPtr (fromIntegral y * fromIntegral pitch + fromIntegral x * fromIntegral bpp) pixelData
